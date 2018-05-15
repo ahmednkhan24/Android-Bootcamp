4fdd37a15b96189587d72b4b543c156dfe4c9192
ahmed <akhan227@uic.edu>
2018-05-15 17:17:52 -0500

    added files

---
+++ Dicee/README.md
@@ -0,0 +1,3 @@
+# Dicee
+
+A simple dice app. The app has two red dice and one button. When the button is clicked the dice app rolls the dice are rolled and output to the screen using a random number generator. 
---
+++ Dicee/app/src/main/java/com/example/faraz/dicee/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.faraz.dicee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ImageView leftDice = (ImageView)findViewById(R.id.image_leftDice);
+        final ImageView rightDice = (ImageView)findViewById(R.id.image_rightDice);
+
+        final Random randNumGen = new Random();
+
+        final int[] diceArr = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6
+        };
+
+        Button rollButton = (Button)findViewById(R.id.rollButton);
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int randNum = randNumGen.nextInt(6);
+                leftDice.setImageResource(diceArr[randNum]);
+
+                randNum = randNumGen.nextInt(6);
+                rightDice.setImageResource(diceArr[randNum]);
+            }
+        });
+    }
+}
